39f5424baebabd39ad269d2b97bf375652864f97
Alex Thompson <ahthomps1@gmail.com>
2021-03-24 15:55:17 -0600

    Change website title

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App" >
-        <h1>Hello</h1>
+        <h1>Rancid Tomatillos</h1>
         <MoviesView movieList={this.state.movies} />
       </div>
     );
--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './MovieCard.css';
 
-const MovieCard = ({ image, title, rating, id, key }) => {
+const MovieCard = ({ image, title, rating, id }) => {
   return (
     <div className='movie-card' id={id}>
-      <img className='movie-image' src={image}></img>
+      <img className='movie-image' src={image} alt={title + ' movie poster'}></img>
       <section className='movie-details'>
         <h2 className='movie-title'>{title}</h2>
-        <p className='movie-rating'>{rating}</p>
+        <p className='movie-rating'>{Math.round(rating * 10)}%</p>
       </section>
     </div>
   )
