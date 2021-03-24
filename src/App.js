3632151703ddd0538017f1cba17be3e47a6eb3d5
Matt McVey <matt.mcvey49@gmail.com>
2021-03-24 16:52:28 -0600

    Add functionality to click on a movie and the matching id to add to the current movie array

--- src/App.js
+++ src/App.js
@@ -7,15 +7,24 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData.movies
+      movies: movieData.movies,
+      currentMovie: []
     }
+  }
+
+  clickedMovie = (id) => {
+    console.log(id)
+    const matchingMovie = this.state.movies.find(movie => id === movie.id)
+    this.setState({ currentMovie: matchingMovie })
   }
 
   render() {
     return (
       <div className="App" >
         <h1>Rancid Tomatillos</h1>
-        <MoviesView movieList={this.state.movies} />
+        {!this.state.currentMovie.length &&
+          <MoviesView movieList={this.state.movies} movieClicked={this.clickedMovie}/>
+        }
       </div>
     );
 
---
+++ src/MovieInfo/MovieInfo.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './MovieInfo.css';
+
+class MovieInfo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return(
+
+    )
+  }
+}
