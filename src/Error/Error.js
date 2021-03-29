d47ca74ab029fa6883e574af684fdc50442c9d89
Alex Thompson <ahthomps1@gmail.com>
2021-03-29 16:09:37 -0600

    Add error and error icon to broken video trailer

--- src/Error/Error.js
+++ src/Error/Error.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './Error.css';
+import brokenVideo from '../images/broken-video.svg';
 
 const Error = ({ error, errorTrailer }) => {
   return (
     <>
       { error &&
-        <h1 className='error'>Opps! Something went wrong, please refresh and try again.</h1>
+        <>
+          <img className='movie-image' src='./images/broken-video.png' alt='server error icon'></img>
+          <h1 className='error'>Opps! Something went wrong, please refresh and try again.</h1>
+        </>
       }
-      {errorTrailer && <h1 className='errorTrailer'>broken</h1>}
+      {errorTrailer &&
+        <div className='error-box'>
+          <img className='error-trailer' src={brokenVideo} alt='broken video image'></img>
+          <p className='error-trailer-message'>Opps! This video isn't working, please refresh and try again</p>
+        </div>
+      }
     </>
   )
 }
