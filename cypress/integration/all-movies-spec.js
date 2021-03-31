6e2074cd7d96a55d479888c1fad7cd912826bae9
Alex Thompson <ahthomps1@gmail.com>
2021-03-31 17:31:43 -0600

    Add sad paths for server and client side errors

--- cypress/integration/all-movies-spec.js
+++ cypress/integration/all-movies-spec.js
@@ -39,34 +39,32 @@ describe('All Movies View', () => {
   });
 });
 
-// describe('All Movies View Erros', () => {
-//   beforeEach(() => {
-//     cy.fixture('movies').then((testMovies) => {
-//       cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', testMovies)
-//     })
-//     cy.visit('http://localhost:3000');
-//   })
+describe('All Movies View Client-Side Error', () => {
+  beforeEach(() => {
 
-//   it('should have a title', () => {
-//     cy.get('h1').contains('Rancid Tomatillos')
-//   });
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 404
+    })
 
-//   it('should not have a home button in the nav', () => {
-//     cy.get('.nav').contains('Home').should('not.exist')
-//   });
+    cy.visit('http://localhost:3000');
+  })
 
-//   it('should display a section containing all the movie posters', () => {
-//     cy.get('.movies-view a').should('have.length', '3')
-//   });
+  it('should display an error message when a data request is broken', () => {
+    cy.get('.error-message').contains('Something went wrong, please refresh and try again.')
+  });
+})
 
-//   it('should display a movies image, title, and rating', () => {
-//     cy.get('.movie-card').contains('Money Plane')
-//     cy.get('.movie-card').contains('49%')
-//     cy.get('.movie-card').children('img').should('have.class', 'movie-image')
-//   });
+describe('All Movies View Errors Server-Side Error', () => {
+  beforeEach(() => {
 
-//   it('should be able to click a movie poster and url will update', () => {
-//     cy.get('.movies-view a').first().click()
-//     cy.url().should('include', 'movies/694919')
-//   });
-// })
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 500
+    })
+
+    cy.visit('http://localhost:3000');
+  })
+
+  it('should display an error message when a data request is broken', () => {
+    cy.get('.error-message').contains('Something went wrong, please refresh and try again.')
+  });
+})
