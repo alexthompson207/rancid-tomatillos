6cf2d026c4662211b708419cbf934d235c87dd7c
Alex Thompson <ahthomps1@gmail.com>
2021-04-01 20:06:30 -0600

    Add conditional in api calls to check for response status

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,16 +1,33 @@
 const getAllMovies = () => {
   return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        return `error`;
+      }
+      return response.json()
+    })
 }
 
 const getMovieByID = (id) => {
   return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        console.log(response)
+        return response.error;
+      }
+      return response.json()
+    })
 }
 
 const getTrailerByID = (id) => {
   return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`)
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        console.log(response)
+        return response.error;
+      }
+      return response.json()
+    })
 }
 
 export { getAllMovies, getMovieByID, getTrailerByID }
